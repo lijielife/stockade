6f491ebea130809ec506015ea330b80c2ce06a7b
Douglas Mendizabal <dougmendizabal@gmail.com>
2013-08-02 12:46:33 -0500

    Fix static files.

--- stockade/settings.py
+++ stockade/settings.py
@@ -69,7 +69,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -80,6 +80,9 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    ('js', os.path.join(PROJECT_DIR, 'static', 'js')),
+    ('css', os.path.join(PROJECT_DIR, 'static', 'css')),
+    ('fonts', os.path.join(PROJECT_DIR, 'static', 'fonts')),
 )
 
 # List of finder classes that know how to find static files in
