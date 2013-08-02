c645e10fbe944cb2fd0d9c7db712bcbb96f2adbd
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-08-02 13:16:43 -0500

    Add the admin site with Project management built in

--- stockade/urls.py
+++ stockade/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('vault.views',
     # Examples:
@@ -13,7 +12,7 @@ urlpatterns = patterns('vault.views',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'projects'),
 	url(r'^login/$', 'login'),
 	url(r'^project/(?P<project_id>\d+)/$', 'project_detail'),
