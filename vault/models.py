8aa958c4998d9a923d7a26b4faafca0f98f94d0e
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-08-02 11:30:59 -0500

    Create vault's models and views

--- vault/models.py
+++ vault/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey('User')
+    description = models.CharField(max_length=255)
+    create_date = models.DateTimeField('date created', auto_now_add=True)
+    modified_date = models.DateTimeField('date created', auto_now_add=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Secret(models.Model):
+    project = models.ForeignKey(Project)
+    category = models.CharField(max_length=200)
+    description = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    secret_ref = models.CharField(max_length=255)
+    create_date = models.DateTimeField('date created', auto_now_add=True)
+    modified_date = models.DateTimeField('date created', auto_now_add=True)
+
+    def __unicode__(self):
+        return self.description
+
+
+class ProjectMember(models.Model):
+    user = models.ForeignKey('User')
+    project models.ForeignKey(Project)
+    create_date = models.DateTimeField('date created', auto_now_add=True)
+    modified_date = models.DateTimeField('date created', auto_now_add=True)
+
--- vault/views.py
+++ vault/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+
+from vault.models import Project
+
+
+def projects(request):
+    project_list = Project.objects.order_by('name')
+    context = {'project_list': project_list}
+    return render(request, 'vault/projects.html', context)
+
+
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    return render(request, 'vault/project.html', {'project': project})
+
+
+def create_project(request, project_id):
+    return render(request, 'vault/project-detail.html')
+
+def login(request):
+    return render(request, 'vault/login.html')
