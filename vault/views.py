97e8256ffd1043d248c35cc9956217598951bf28
Paul Kehrer <paul.l.kehrer@gmail.com>
2013-08-02 13:06:48 -0500

    more template changes and shifts. commits abound

--- vault/views.py
+++ vault/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 
 from vault.models import Project
 
@@ -8,16 +9,19 @@ from vault.models import Project
 def projects(request):
     project_list = Project.objects.order_by('name')
     context = {'project_list': project_list}
-    return render(request, 'vault/projects.html', context)
+    return render_to_response('vault/projects.html', context,
+            context_instance=RequestContext(request))
 
 
 def project_detail(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
-    return render(request, 'vault/project.html', {'project': project})
+    return render_to_response('vault/project.html', {'project': project},
+            context_instance=RequestContext(request))
 
 
 def create_project(request):
-    return render(request, 'vault/project-detail.html')
+    return render_to_response('vault/project-dialog.html',
+        context_instance=RequestContext(request))
 
 
 def login(request):
