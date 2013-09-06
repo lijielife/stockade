a3eafc8ffcd39deaac0394f9c9c3d637e9c4c6b5
evenamonkey <doug.sims@gmail.com>
2013-09-06 13:55:03 -0500

    adding auth-n and auth-z

--- vault/api.py
+++ vault/api.py
@@ -1,7 +1,10 @@
-# myapp/api.py
+
 from tastypie.resources import ModelResource
 from models import Project, Secret, ProjectMember
 from django.contrib.auth.models import User
+from tastypie.authentication import BasicAuthentication, ApiKeyAuthentication, MultiAuthentication
+from tastypie.authorization import DjangoAuthorization
+
 
 
 
@@ -9,21 +12,30 @@ class ProjectResource(ModelResource):
     class Meta:
         queryset = Project.objects.all()
         resource_name = 'projects'
+        authentication = MultiAuthentication(BasicAuthentication(), ApiKeyAuthentication())
+        authorization = DjangoAuthorization()
 
 
 class SecretResource(ModelResource):
     class Meta:
         queryset = Secret.objects.all()
         resource_name = 'secrets'
+        authentication = MultiAuthentication(BasicAuthentication(), ApiKeyAuthentication())
+        authorization = DjangoAuthorization()
 
 
 class ProjectMemberResource(ModelResource):
     class Meta:
         queryset = ProjectMember.objects.all()
         resource_name = 'memberships'
+        authentication = MultiAuthentication(BasicAuthentication(), ApiKeyAuthentication())
+        authorization = DjangoAuthorization()
 
 
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'users'
+        authentication = MultiAuthentication(BasicAuthentication(), ApiKeyAuthentication())
+        authorization = DjangoAuthorization()
+
